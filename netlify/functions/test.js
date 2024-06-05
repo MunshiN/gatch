29957c5d87afefa0e41109009c782ba8edaedbd1
Nishant_renxo <nishant@renxo.tech>
2024-06-05 10:52:11 +0530

    gatch link

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-app.get('/redirect', (req, res) => {
+app.get('/', (req, res) => {
     const userAgent = req.headers['user-agent'];
     if (/android/i.test(userAgent)) {
         res.redirect('https://play.google.com/store/apps/details?id=com.example.app');
@@ -13,10 +13,6 @@ app.get('/redirect', (req, res) => {
     } else {
         res.redirect('https://google.com'); // Fallback URL
     }
-});
-
-app.get('/', (req, res) => {
-    res.redirect('/.netlify/functions/redirect'); // Redirect to the function path
 });
 
 module.exports.handler = serverless(app);
