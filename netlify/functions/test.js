ce41d045c2403b3ff8362bc50862fae60246d7f0
Nishant_renxo <nishant@renxo.tech>
2024-06-05 12:30:44 +0530

    gatch link

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -7,9 +7,9 @@ const app = express();
 app.get('/', (req, res) => {
     const userAgent = req.headers['user-agent'];
     if (/android/i.test(userAgent)) {
-        res.redirect('https://play.google.com/store/apps/details?id=com.example.app');
+        res.redirect('https://play.google.com/store/apps/details?id=com.lutsgames.IdleSuperpowers&pcampaignid');
     } else if (/iphone|ipad|ipod/i.test(userAgent)) {
-        res.redirect('https://apps.apple.com/us/app/example-app/id1234567890');
+        res.redirect('https://apps.apple.com/us/app/keynote/id361285480');
     } else {
         res.redirect('https://google.com'); // Fallback URL
     }
